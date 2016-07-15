b0fb2c81ce85f941d97b03944f3f2ffade4454f4
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-15 23:57:05 +0800

    add Image project

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// Hello world start
+
+// import React, { Component } from 'react';
+// import { AppRegistry, Text } from 'react-native';
+
+// class HelloWorldApp extends Component {
+//   render() {
+//     return (
+//       <Text>!</Text>
+//     );
+//   }
+// }
+
+// AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
+
+// Hello world end
+
+
+/////////////////////////////////////
+// Image start
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
-class AwesomeProject extends Component {
+class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+// Image end
